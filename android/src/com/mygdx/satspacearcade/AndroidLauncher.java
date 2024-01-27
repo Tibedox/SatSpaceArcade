743066173a2df61aa6c71ee1bab5cbf2a7c82ccb
teacher <tibedox@mail.ru>
2024-01-27 18:20:21 +0600

    Start project

---
+++ android/src/com/mygdx/satspacearcade/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.satspacearcade;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.satspacearcade.SatSpaceArcade;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SatSpaceArcade(), config);
+	}
+}
