e3366d17770d87e33775f4a3e194e389eb138cbe
teacher <tibedox@mail.ru>
2024-02-03 18:35:56 +0600

    Ship 2

--- core/src/com/mygdx/satspacearcade/Ship.java
+++ core/src/com/mygdx/satspacearcade/Ship.java
@@ -3,11 +3,7 @@ package com.mygdx.satspacearcade;
 import static com.mygdx.satspacearcade.SatSpaceArcade.SCR_HEIGHT;
 import static com.mygdx.satspacearcade.SatSpaceArcade.SCR_WIDTH;
 
-import com.badlogic.gdx.utils.TimeUtils;
-
 public class Ship extends SpaceObject{
-    int phase, nPhases = 12;
-    long timeLastPhase, timePhaseInterval = 50;
 
     public Ship(){
         x = SCR_WIDTH/2;
@@ -18,13 +14,5 @@ public class Ship extends SpaceObject{
     @Override
     void move() {
         super.move();
-        changePhase();
-    }
-
-    void changePhase(){
-        if(TimeUtils.millis() > timeLastPhase+timePhaseInterval) {
-            if (++phase == nPhases) phase = 0;
-            timeLastPhase = TimeUtils.millis();
-        }
     }
 }
--- core/src/com/mygdx/satspacearcade/SpaceObject.java
+++ core/src/com/mygdx/satspacearcade/SpaceObject.java
@@ -1,13 +1,18 @@
 package com.mygdx.satspacearcade;
 
+import com.badlogic.gdx.utils.TimeUtils;
+
 public class SpaceObject {
     float x, y;
     float width, height;
     float vx, vy;
+    int phase, nPhases = 12;
+    long timeLastPhase, timePhaseInterval = 50;
 
     void move() {
         x += vx;
         y += vy;
+        changePhase();
     }
 
     float getX(){
@@ -15,5 +20,12 @@ public class SpaceObject {
     }
     float getY(){
         return y-height/2;
+    }
+
+    void changePhase(){
+        if(TimeUtils.millis() > timeLastPhase+timePhaseInterval) {
+            if (++phase == nPhases) phase = 0;
+            timeLastPhase = TimeUtils.millis();
+        }
     }
 }
